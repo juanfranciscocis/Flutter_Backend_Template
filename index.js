5e531a8362183d4948af7058de20e723f4d4a514
juancisneros <juanfrancistm2011@icloud.com>
2023-01-09 13:14:52 -0500

    Backend Template

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ app.use( express.static( publicPath ) );
 
 
 
-server.listen( process.env.PORT, ( err ) => {
+server.listen( 3000, ( err ) => {
 
     if ( err ) throw new Error(err);
 
-    console.log('Servidor corriendo en puerto', process.env.PORT );
+    console.log('Servidor corriendo en puerto', 3000 );
 
 });
 
